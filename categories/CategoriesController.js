1b210af85ed2a9b78fbe97ed950cf050ab6889b5
kauessz <ssz.kaue@gmail.com>
2022-07-28 21:21:40 -0300

    ajuste excluir categoria

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -5,50 +5,49 @@ const category = require('./Category');
 //carrego o slugify
 const slugify = require('slugify');
 
-router.get('/admin/categories/new',(req,res) => {
-    res.render('admin/categories/new');
-})
+router.get("/admin/categories/new",(req, res) => {
+    res.render("admin/categories/new");
+});
 
-router.post('/categories/save',(req,res) => {
-    let title = req.body.title
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
     if(title != undefined){
+        
         category.create({
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect('/');
+            res.redirect("/");
         })
+
     }else{
-        res.redirect('/admin/categories/new');
+        res.redirect("/admin/categories/new");
     }
 });
 
-router.get('/admin/categories',(req,res) => {
+router.get("/admin/categories",  (req, res) => {
     category.findAll().then(categories => {
-        res.render('admin/categories/index',{categories: categories});
+        res.render("admin/categories/index", {categories: categories});
     });
 });
 
-router.post('/categories/delete',(req,res) => {
-    let id = req.body.id;
-
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
     if(id != undefined){
         if(!isNaN(id)){
             category.destroy({
                 where: {
                     id: id
                 }
-            }).then(()=> {
-                res.redirect('/admin/categories')
+            }).then(() => {
+                res.redirect("/admin/categories");
             });
-        }else{
-            res.redirect('/admin/categories')
+        }else{// NÃO FOR UM NÚMERO
+            res.redirect("/admin/categories");
         }
-    }else{
-        res.redirect('/admin/categories')
+    }else{ // NULL
+        res.redirect("/admin/categories");
     }
 });
 
 module.exports = router;
-
-//console.log(title)
